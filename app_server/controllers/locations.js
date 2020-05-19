718f1fb239a5c7f189334748177bea38efd2b1c0
John Lenon Nascimento da Silva <jnascimento@itelios.com>
2020-05-19 13:49:21 -0300

    Updating application 17-05-2020

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,7 +2,41 @@
     // Get Homepage page
     homelist: function(req, res, next) {
         res.render('locations-list', {
-            title: 'Locations list page'
+            title: 'Loc8r - find a place to work with wifi',
+            pageHeader: {
+                title: 'Loc8r',
+                strapline: 'Find places to work with wifi near you!'
+            },
+            locations: [
+                {
+                    name: 'Starcups',
+                    address: 'A 125 Hight street, Reading, RG6 1PS',
+                    rating: 3,
+                    facilities: ['Breakfast', 'Food', 'Premium wi-fi'],
+                    distance: '100m'
+                },
+                {
+                    name: 'Cafe Hero',
+                    address: 'B 125 High Street, Reading, RG6 1PS',
+                    rating: 4,
+                    facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+                    distance: '200m'
+                },
+                {
+                    name: 'Burger Queen',
+                    address: 'C 125 High Street, Reading, RG6 1PS',
+                    rating: 2,
+                    facilities: ['Food', 'Premium wifi'],
+                    distance: '250m'
+                },
+                {
+                    name: 'Burger Queen',
+                    address: 'C 125 High Street, Reading, RG6 1PS',
+                    rating: 0,
+                    facilities: ['Food', 'Premium wifi'],
+                    distance: '250m'
+                }
+            ]
         });
     },
 
